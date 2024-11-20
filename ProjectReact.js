ed0615f317f3f2cca14b499888b10fa46fe57d14
DesignerDeveloperDove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2024-11-19 19:19:14 -0500

    try again

--- ProjectReact.js
+++ ProjectReact.js
@@ -2,3 +2,5 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
   location.replace(location.href);
+<script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.44/build/spline-viewer.js"></script>
+
