82210ad8c08a7e125b5e5315ba88824d4d9b289f
DesignerDeveloperDove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2024-11-21 04:08:41 -0500

    Commenting out old for a sec

--- Fall 2024 copy/Front end II/React Project copy/ReactProject.js
+++ Fall 2024 copy/Front end II/React Project copy/ReactProject.js
@@ -1,9 +1,10 @@
 
  // use a script tag or an external JS file
- document.addEventListener("DOMContentLoaded", (event) => {
+/* document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger)
   // gsap code here!
- });
+ }); 
+ */
 gsap.fromTo("#SubHeader #b",{opacity:0},{opacity:1,duration:5});
 
   gsap.fromTo("RightSide",{x:500},{y:1200,duration:10});
