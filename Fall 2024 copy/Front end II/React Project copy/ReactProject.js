5811f288e47deab3c4f349e2dd47d917a21022d8
DesignerDeveloperDove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2024-11-21 03:55:09 -0500

    semi

--- Fall 2024 copy/Front end II/React Project copy/ReactProject.js
+++ Fall 2024 copy/Front end II/React Project copy/ReactProject.js
@@ -6,7 +6,7 @@
  });
 gsap.fromto("#SubHeader #b",{opacity:0},{opacity:1,duration:5});
 
-  gsap.fromto("RightSide",{x:500},{y:1200,duration:10})
+  gsap.fromto("RightSide",{x:500},{y:1200,duration:10});
 console.log("test");
 
 
