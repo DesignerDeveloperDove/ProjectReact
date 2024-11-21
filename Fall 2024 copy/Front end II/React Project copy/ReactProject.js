9794d1840931caf98f39575f9a5329f2a6fd4ef9
DesignerDeveloperDove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2024-11-21 03:43:39 -0500

    Update ReactProject.js

--- Fall 2024 copy/Front end II/React Project copy/ReactProject.js
+++ Fall 2024 copy/Front end II/React Project copy/ReactProject.js
@@ -1,1 +1,15 @@
 
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+ });
+gsap.fromto("#SubHeader #b",{opacity:0},{opacity:1,duration:5});
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+  location.replace(location.href);
+<script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.44/build/spline-viewer.js"></script>
+
+
