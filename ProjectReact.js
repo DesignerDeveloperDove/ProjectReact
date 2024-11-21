1acb173c90b449b3eddaea42aa17da10143b1ed3
DesignerDeveloperDove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2024-11-21 03:27:17 -0500

    Scroll trigger added

--- ProjectReact.js
+++ ProjectReact.js
@@ -1,3 +1,11 @@
+
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+ });
+
+
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
